15532d0ad9809bd14c53d382acf3e882184a0ec4
Desteel <AlexGoWargame@yandex.ru>
2023-12-12 23:21:55 +0200

    Add `connect`

--- app/lib/connect.tsx
+++ app/lib/connect.tsx
@@ -1,13 +1,16 @@
 import { ComponentType } from 'react';
 
-export function connect<Props extends Record<string, unknown> = {}>(
-  Component: ComponentType<Props>,
-  propsGetter: () => Promise<Props>,
-) {
-  async function ConnectedComponent() {
-    const props = await propsGetter();
+export function connect<
+  Props extends Record<string, unknown> = {},
+  GettedProps extends Partial<Props> = {},
+>(Component: ComponentType<Props>, propsGetter: () => Promise<GettedProps>) {
+  async function ConnectedComponent(props: Omit<Props, keyof GettedProps>) {
+    const actualProps = {
+      ...props,
+      ...(await propsGetter()),
+    } as Props;
 
-    return <Component {...props} />;
+    return <Component {...actualProps} />;
   }
 
   ConnectedComponent.displayName = `Connected${
